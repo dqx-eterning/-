60b8bbce127bcec105e45e208e77fbb69d29ff38
dqx_eterning <1986813958@qq.com>
2020-09-19 22:17:40 +0800

    hanming

---
+++ src/main/java/汉明距离.java
@@ -0,0 +1,12 @@
+public class 汉明距离 {
+    public static void main(String[] args) {
+        int x =1 ;
+        int y = 4;
+        System.out.println(hammingDistance(x,y));
+    }
+
+    public static int hammingDistance(int x, int y) {
+
+       return Integer.bitCount(x ^ y);
+    }
+}

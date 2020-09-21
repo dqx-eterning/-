401993f5d0544afcd8d84ca6c906805fa0635a6c
dqx_eterning <1986813958@qq.com>
2020-09-21 22:29:12 +0800

    test

--- src/main/java/重复的子字符串.java
+++ src/main/java/重复的子字符串.java
@@ -1,24 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class 重复的子字符串 {
     public static void main(String[] args) {
-        String a = "ababba";
+        String a = "abcabcabc";
         boolean b = repeatedSubstringPattern(a);
         System.out.println(b);
 
     }
     public  static boolean repeatedSubstringPattern(String s) {
-        if (s.length() == 1 || s.length() % 2 == 1){
+        if (s.length() == 1 ) {
             return false;
         }
+
         for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(0)){
-                for (int j = 0; j < i; j++) {
-                    if (s.charAt(j) != s.charAt(i+j)){
-                        return false;
+            boolean tag = true;
+            if ( s.length() % i == 0){
+                String s1 = s.substring(0,i);
+                for (int j = i; j + i <= s.length(); j=j+i) {
+                    if ( !s.substring(j,j+i).equals(s1)){
+                        tag = false;
                     }
                 }
-                if (i == s.length()-1)
-                return true;
+                if (tag){
+                    return true;
+                }
+
             }
         }
         return false;

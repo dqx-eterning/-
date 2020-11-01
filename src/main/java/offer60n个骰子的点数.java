2ade83e0b46e3b485094d43932139bedd8e39db3
dqx_eterning <1986813958@qq.com>
2020-11-01 22:57:13 +0800

    1101

---
+++ src/main/java/offer60n个骰子的点数.java
@@ -0,0 +1,19 @@
+public class offer60n个骰子的点数 {
+    public static void main(String[] args) {
+
+    }
+    //这题着实不算简单
+    //没看懂
+    public double[] twoSum(int n) {
+        double pre[]={1/6d,1/6d,1/6d,1/6d,1/6d,1/6d};
+        for(int i=2;i<=n;i++){
+            double tmp[]=new double[5*i+1];
+            for(int j=0;j<pre.length;j++)
+                for(int x=0;x<6;x++)
+                    tmp[j+x]+=pre[j]/6;
+            pre=tmp;
+        }
+        return pre;
+    }
+
+}

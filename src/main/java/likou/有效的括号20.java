f493d48ffa02d53e4d5f404c2c9e6bed0669d452
dqx_eterning <1986813958@qq.com>
2020-11-04 23:23:36 +0800

    1104

---
+++ src/main/java/likou/有效的括号20.java
@@ -0,0 +1,31 @@
+package likou;
+
+import java.util.Stack;
+
+public class 有效的括号20 {
+    public boolean isValid(String s) {
+        if(s.length()%2==0){
+            Stack<Character> stack=new Stack<>();
+            for(int i=0;i<s.length();i++){
+                char c=s.charAt(i);
+                if(c=='('||c=='['||c=='{'){
+                    stack.push(c);
+                }
+                else if(c==')'){
+                    if(!stack.isEmpty()&&stack.peek()=='('){stack.pop();}
+                    else{return false;}
+                }
+                else if(c=='}'){
+                    if(!stack.isEmpty()&&stack.peek()=='{'){stack.pop();}
+                    else{return false;}
+                }
+                else{
+                    if(!stack.isEmpty()&&stack.peek()=='['){stack.pop();}
+                    else{return false;}
+                }
+            }
+            return stack.isEmpty();
+        }
+        return false;
+    }
+}
